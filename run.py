ca0616e79ebeedd8b280e854d48bdbec615e2e65
bass <gtluu912@gmail.com>
2023-09-22 13:43:48 -0700

    refactor to use pyTDFSDK and timsconvert packages

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
 import argparse
 import lxml.etree as et
-from timsconvert.init_bruker_dll import *
-from timsconvert.data_input import *
-from timsconvert.classes import *
-from timsconvert.parse_maldi import *
+import numpy as np
+import pandas as pd
+from pyTDFSDK.init_tdf_sdk import init_tdf_sdk_api
 from pymaldiproc.classes import *
 from pymaldiproc.preprocessing import *
+from timsconvert.classes import TimsconvertTsfData, TimsconvertTdfData
+from timsconvert.data_input import schema_detection
+from timsconvert.parse import parse_maldi_tsf, parse_maldi_tdf
 
 
 def arg_descriptions():
@@ -148,8 +150,53 @@ def get_args():
     return vars(arguments)
 
 
-def main(args):
-    sdk = init_tdf_sdk_dll()
+def preprocess(maldi_spectra, args):
+    if args['trim_spectra']:
+        maldi_spectra = trim_spectra(maldi_spectra,
+                                     lower_mass_range=args['trim_spectra_lower_mass_range'],
+                                     upper_mass_range=args['trim_spectra_upper_mass_range'])
+    if args['transform_intensity']:
+        maldi_spectra = transform_intensity(maldi_spectra, method=args['transform_intensity_method'])
+    if args['smooth_baseline']:
+        maldi_spectra = smooth_baseline(maldi_spectra,
+                                        method=args['smooth_baseline_method'],
+                                        window_length=args['smooth_baseline_window_length'],
+                                        polyorder=args['smooth_baseline_polyorder'],
+                                        delta_mz=args['smooth_baseline_delta_mz'],
+                                        diff_thresh=args['smooth_baseline_diff_thresh'])
+    if args['remove_baseline']:
+        maldi_spectra = remove_baseline(maldi_spectra,
+                                        method=args['remove_baseline_method'],
+                                        lambda_=args['remove_baseline_lambda'],
+                                        porder=args['remove_baseline_polyorder'],
+                                        repitition=args['remove_baseline_repitition'],
+                                        degree=args['remove_baseline_degree'],
+                                        gradient=args['remove_baseline_gradient'])
+    if args['normalize_intensity']:
+        maldi_spectra = normalize_intensity(maldi_spectra, method=args['normalize_intensity_method'])
+    if args['bin_spectra']:
+        maldi_spectra = bin_spectra(maldi_spectra,
+                                    n_bins=args['bin_spectra_n_bins'],
+                                    lower_mass_range=args['bin_spectra_lower_mass_range'],
+                                    upper_mass_range=args['bin_spectra_upper_mass_range'])
+    if args['align_spectra']:
+        maldi_spectra = align_spectra(maldi_spectra,
+                                      method=args['align_spectra_method'],
+                                      inter=args['align_spectra_inter'],
+                                      n=args['align_spectra_n'],
+                                      scale=args['align_spectra_scale'],
+                                      coshift_preprocessing=args['align_spectra_coshift_preprocessing'],
+                                      coshift_preprocessing_max_shift=args[
+                                          'align_spectra_coshift_preprocessing_max_shift'],
+                                      fill_with_previous=args['align_spectra_fill_with_previous'],
+                                      average2_multiplier=args['align_spectra_average2_multiplier'])
+    return maldi_spectra
+
+
+def main():
+    args = get_args()
+
+    sdk = init_tdf_sdk_api()
 
     if args['outdir'] == '':
         outdir = os.path.split(args['autox'])[0]
@@ -165,12 +212,12 @@ def main(args):
             if spot_group.attrib['sampleName'] == args['blank']:
                 dot_d_path = os.path.join(autox.attrib['directory'], spot_group.attrib['sampleName'] + '.d')
                 if schema_detection(dot_d_path) == 'TSF':
-                    blank = tsf_data(dot_d_path, sdk)
-                    num_frames = blank.frames.shape[0] + 1
+                    blank = TimsconvertTsfData(dot_d_path, sdk)
+                    num_frames = blank.analysis['Frames'].shape[0] + 1
                     scan_dicts = parse_maldi_tsf(blank, 1, num_frames, 'centroid', False, 0, 64)
                 elif schema_detection(dot_d_path) == 'TDF':
-                    blank = tdf_data(dot_d_path, sdk)
-                    num_frames = blank.frames.shape[0] + 1
+                    blank = TimsconvertTdfData(dot_d_path, sdk)
+                    num_frames = blank.analysis['Frames'].shape[0] + 1
                     scan_dicts = parse_maldi_tdf(blank, 1, num_frames, 'centroid', False, True, 0, 64)
                 maldi_spectra = []
                 for count, scan_dict in enumerate(scan_dicts):
@@ -180,44 +227,7 @@ def main(args):
                     scan_dict['m/z array'] = scan_dict['mz_array']
                     scan_dict['intensity array'] = scan_dict['intensity_array']
                     maldi_spectra.append(MALDISpectrum(scan_dict, dot_d_path))
-                if args['trim_spectra']:
-                    maldi_spectra = trim_spectra(maldi_spectra,
-                                                 lower_mass_range=args['trim_spectra_lower_mass_range'],
-                                                 upper_mass_range=args['trim_spectra_upper_mass_range'])
-                if args['transform_intensity']:
-                    maldi_spectra = transform_intensity(maldi_spectra, method=args['transform_intensity_method'])
-                if args['smooth_baseline']:
-                    maldi_spectra = smooth_baseline(maldi_spectra,
-                                                    method=args['smooth_baseline_method'],
-                                                    window_length=args['smooth_baseline_window_length'],
-                                                    polyorder=args['smooth_baseline_polyorder'],
-                                                    delta_mz=args['smooth_baseline_delta_mz'],
-                                                    diff_thresh=args['smooth_baseline_diff_thresh'])
-                if args['remove_baseline']:
-                    maldi_spectra = remove_baseline(maldi_spectra,
-                                                    method=args['remove_baseline_method'],
-                                                    lambda_=args['remove_baseline_lambda'],
-                                                    porder=args['remove_baseline_polyorder'],
-                                                    repitition=args['remove_baseline_repitition'],
-                                                    degree=args['remove_baseline_degree'],
-                                                    gradient=args['remove_baseline_gradient'])
-                if args['normalize_intensity']:
-                    maldi_spectra = normalize_intensity(maldi_spectra, method=args['normalize_intensity_method'])
-                if args['bin_spectra']:
-                    maldi_spectra = bin_spectra(maldi_spectra,
-                                                n_bins=args['bin_spectra_n_bins'],
-                                                lower_mass_range=args['bin_spectra_lower_mass_range'],
-                                                upper_mass_range=args['bin_spectra_upper_mass_range'])
-                if args['align_spectra']:
-                    maldi_spectra = align_spectra(maldi_spectra,
-                                                  method=args['align_spectra_method'],
-                                                  inter=args['align_spectra_inter'],
-                                                  n=args['align_spectra_n'],
-                                                  scale=args['align_spectra_scale'],
-                                                  coshift_preprocessing=args['align_spectra_coshift_preprocessing'],
-                                                  coshift_preprocessing_max_shift=args['align_spectra_coshift_preprocessing_max_shift'],
-                                                  fill_with_previous=args['align_spectra_fill_with_previous'],
-                                                  average2_multiplier=args['align_spectra_average2_multiplier'])
+                maldi_spectra = preprocess(maldi_spectra, args)
                 maldi_spectra = peak_picking(maldi_spectra,
                                              method=args['peak_picking_method'],
                                              widths=args['peak_picking_widths'],
@@ -226,18 +236,17 @@ def main(args):
                 feature_matrix = feature_matrix.round(2)
                 exclusion_list_df = pd.DataFrame(data={'mz': np.unique(feature_matrix['mz'].values)})
 
-    # TODO: initialize new etree here
     new_autox = et.Element(autox.tag, attrib=autox.attrib)
     for spot_group in autox:
         if spot_group.attrib['sampleName'] != args['blank']:
             dot_d_path = os.path.join(autox.attrib['directory'], spot_group.attrib['sampleName'] + '.d')
             if schema_detection(dot_d_path) == 'TSF':
-                data = tsf_data(dot_d_path, sdk)
-                num_frames = data.frames.shape[0] + 1
+                data = TimsconvertTsfData(dot_d_path, sdk)
+                num_frames = data.analysis['Frames'].shape[0] + 1
                 scan_dicts = parse_maldi_tsf(data, 1, num_frames, 'centroid', False, 0, 64)
             elif schema_detection(dot_d_path) == 'TDF':
-                data = tdf_data(dot_d_path, sdk)
-                num_frames = data.frames.shape[0] + 1
+                data = TimsconvertTdfData(dot_d_path, sdk)
+                num_frames = data.analysis['Frames'].shape[0] + 1
                 scan_dicts = parse_maldi_tdf(data, 1, num_frames, 'centroid', False, True, 0, 64)
             maldi_spectra = []
             for count, scan_dict in enumerate(scan_dicts):
@@ -247,48 +256,9 @@ def main(args):
                 scan_dict['m/z array'] = scan_dict['mz_array']
                 scan_dict['intensity array'] = scan_dict['intensity_array']
                 maldi_spectra.append(MALDISpectrum(scan_dict, dot_d_path))
-            if args['trim_spectra']:
-                maldi_spectra = trim_spectra(maldi_spectra,
-                                             lower_mass_range=args['trim_spectra_lower_mass_range'],
-                                             upper_mass_range=args['trim_spectra_upper_mass_range'])
-            if args['transform_intensity']:
-                maldi_spectra = transform_intensity(maldi_spectra, method=args['transform_intensity_method'])
-            if args['smooth_baseline']:
-                maldi_spectra = smooth_baseline(maldi_spectra,
-                                                method=args['smooth_baseline_method'],
-                                                window_length=args['smooth_baseline_window_length'],
-                                                polyorder=args['smooth_baseline_polyorder'],
-                                                delta_mz=args['smooth_baseline_delta_mz'],
-                                                diff_thresh=args['smooth_baseline_diff_thresh'])
-            if args['remove_baseline']:
-                maldi_spectra = remove_baseline(maldi_spectra,
-                                                method=args['remove_baseline_method'],
-                                                lambda_=args['remove_baseline_lambda'],
-                                                porder=args['remove_baseline_polyorder'],
-                                                repitition=args['remove_baseline_repitition'],
-                                                degree=args['remove_baseline_degree'],
-                                                gradient=args['remove_baseline_gradient'])
-            if args['normalize_intensity']:
-                maldi_spectra = normalize_intensity(maldi_spectra, method=args['normalize_intensity_method'])
-            if args['bin_spectra']:
-                maldi_spectra = bin_spectra(maldi_spectra,
-                                            n_bins=args['bin_spectra_n_bins'],
-                                            lower_mass_range=args['bin_spectra_lower_mass_range'],
-                                            upper_mass_range=args['bin_spectra_upper_mass_range'])
-            if args['align_spectra']:
-                maldi_spectra = align_spectra(maldi_spectra,
-                                              method=args['align_spectra_method'],
-                                              inter=args['align_spectra_inter'],
-                                              n=args['align_spectra_n'],
-                                              scale=args['align_spectra_scale'],
-                                              coshift_preprocessing=args['align_spectra_coshift_preprocessing'],
-                                              coshift_preprocessing_max_shift=args[
-                                                  'align_spectra_coshift_preprocessing_max_shift'],
-                                              fill_with_previous=args['align_spectra_fill_with_previous'],
-                                              average2_multiplier=args['align_spectra_average2_multiplier'])
+            maldi_spectra = preprocess(maldi_spectra, args)
             for cont in spot_group:
                 spectrum = [i for i in maldi_spectra if i.spot == cont.attrib['Pos_on_Scout']][0]
-                # TODO: make pd df and get top n intensity features
                 spectrum = peak_picking([spectrum],
                                         method=args['peak_picking_method'],
                                         widths=args['peak_picking_widths'],
@@ -330,5 +300,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    args = get_args()
-    main(args)
+    main()
