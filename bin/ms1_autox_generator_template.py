cb9f11250a3c1d6de9ff284abbfa7eb56fadcb64
gtluu <gtluu912@gmail.com>
2024-03-24 19:30:43 -0500

    update imports

--- bin/ms1_autox_generator_template.py
+++ bin/ms1_autox_generator_template.py
@@ -8,18 +8,10 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
-    QCursor, QFont, QFontDatabase, QGradient,
-    QIcon, QImage, QKeySequence, QLinearGradient,
-    QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QHeaderView, QLabel,
-    QLineEdit, QMainWindow, QMenu, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTableWidget,
-    QTableWidgetItem, QWidget)
+from PySide6.QtCore import QCoreApplication, QMetaObject, QRect, QSize
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import (QComboBox, QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+                               QTableWidget, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
