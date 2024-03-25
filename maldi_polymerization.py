c484877e4306b7c3adb624bae89c6aa1ef9157f4
gtluu <gtluu912@gmail.com>
2024-03-25 18:36:56 -0500

    add maldi_polymerization script

---
+++ maldi_polymerization.py
@@ -0,0 +1,242 @@
+import os
+import sys
+import copy
+import gc
+from psims.mzml import MzMLWriter
+from pyTDFSDK.init_tdf_sdk import *
+from pyTDFSDK.classes import *
+
+# Code modified from TIMSCONVERT 1.6.5.
+
+INSTRUMENT_SOURCE_TYPE = {'0': 'unspecified',
+                          '1': 'electrospray ionization',
+                          '2': 'atmospheric pressure chemical ionization',
+                          '3': 'nanoelectrospray',
+                          '4': 'nanoelectrospray',
+                          '5': 'atmospheric pressure photoionization',
+                          '6': 'multimode ionization',
+                          '9': 'nanoflow electrospray ionization',
+                          '10': 'ionBooster',
+                          '11': 'CaptiveSpray',
+                          '12': 'GC-APCI',
+                          '13': 'VIP-HESI-APCI',
+                          '18': 'VIP-HESI'}
+
+
+class PartialTsfData(TsfData):
+    def __init__(self, bruker_d_folder_name, tdf_sdk, use_recalibrated_state=True):
+        super().__init__(bruker_d_folder_name, tdf_sdk, use_recalibrated_state)
+        self.MzAcqRangeLower = float(self.analysis['GlobalMetadata']['MzAcqRangeLower'])
+        self.MzAcqRangeUpper = float(self.analysis['GlobalMetadata']['MzAcqRangeUpper'])
+
+
+def trim_spectrum(spectrum, lower_mass_range, upper_mass_range):
+    indices = np.where((spectrum.mz_array > lower_mass_range) & (spectrum.mz_array <= upper_mass_range))[0]
+    spectrum.mz_array = copy.deepcopy(spectrum.mz_array[indices])
+    spectrum.intensity_array = copy.deepcopy(spectrum.intensity_array[indices])
+    gc.collect()
+    return spectrum
+
+
+def i_activate_the_magic_card_polymerization(sorted_dlist_lower, frame, mode, profile_bins, encoding):
+    spectra = [(TsfSpectrum(data, frame, mode, profile_bins, encoding),
+                data)
+               for data in sorted_dlist_lower]
+    spectra = [(trim_spectrum(spectrum, data.MzAcqRangeLower, data.MzAcqRangeUpper),
+                data)
+               for spectrum, data in spectra]
+    mz_arrays = [copy.deepcopy(spectrum.mz_array) for spectrum, data in spectra]
+    intensity_arrays = [copy.deepcopy(spectrum.intensity_array)
+                        for spectrum, data in spectra]
+    stitched_mz_array = np.concatenate(mz_arrays)
+    stitched_intensity_array = np.concatenate(intensity_arrays)
+    return stitched_mz_array, stitched_intensity_array
+
+
+def write_stitched_mzml_metadata(writer, dlist, filenames, mode, barebones_metadata):
+    # Write controlled vocabularies
+    writer.controlled_vocabularies()
+    # Basic file descriptions.
+    file_description = ['MS1 spectrum']
+    if mode == 'raw' or mode == 'centroid':
+        file_description.append('centroid spectrum')
+    elif mode == 'profile':
+        file_description.append('profile spectrum')
+    # Source File
+    sf = [writer.SourceFile(os.path.split(i)[0],
+                            os.path.split(i)[1],
+                            id=str(count))
+          for count, i in enumerate(filenames)]
+    writer.file_description(file_contents=file_description, source_files=sf)
+    # Add list of software.
+    if not barebones_metadata:
+        acquisition_software = {'id': 'micrOTOFcontrol',
+                                'version': dlist[0].analysis['GlobalMetadata']['AcquisitionSoftwareVersion'],
+                                'params': ['micrOTOFcontrol', ]}
+        tdf_sdk_software = {'id': 'TDF-SDK',
+                            'version': '2.21.0.4',
+                            'params': ['Bruker software']}
+        psims_software = {'id': 'psims-writer',
+                          'version': '1.2.7',
+                          'params': ['python-psims', ]}
+        flex_maldi_dda_automation_software = {'id': 'flex_maldi_dda_automation',
+                                              'version': '0.4.0a1',
+                                              'params': ['flex_maldi_dda_automation', ]}
+        writer.software_list([acquisition_software,
+                              tdf_sdk_software,
+                              psims_software,
+                              flex_maldi_dda_automation_software])
+    # Instrument configuration.
+    # Instrument source, analyzer, and detector are all hard coded to timsTOF hardware and does not allow for non-stock
+    # configurations.
+    inst_count = 1
+    if dlist[0].analysis['GlobalMetadata']['InstrumentSourceType'] in INSTRUMENT_SOURCE_TYPE.keys() \
+            and 'MaldiApplicationType' not in dlist[0].analysis['GlobalMetadata'].keys():
+        source = writer.Source(inst_count,
+                               [INSTRUMENT_SOURCE_TYPE[dlist[0].analysis['GlobalMetadata']['InstrumentSourceType']]])
+    elif 'MaldiApplicationType' in dlist[0].analysis['GlobalMetadata'].keys():
+        source = writer.Source(inst_count, ['matrix-assisted laser desorption ionization'])
+    # Analyzer and detector hard coded for timsTOF fleX
+    inst_count += 1
+    analyzer = writer.Analyzer(inst_count, ['quadrupole', 'time-of-flight'])
+    inst_count += 1
+    detector = writer.Detector(inst_count, ['microchannel plate detector', 'photomultiplier'])
+    # Get instrument serial number.
+    serial_number = dlist[0].analysis['GlobalMetadata']['InstrumentSerialNumber']
+    # Get instrument name based on GlobalMetadata or Properties table.
+    if not barebones_metadata:
+        instrument_name = dlist[0].analysis['GlobalMetadata']['InstrumentName'].strip().lower()
+        if instrument_name == 'timstof':
+            instrument_name = 'timsTOF'
+        elif instrument_name == 'timstof pro':
+            instrument_name = 'timsTOF Pro'
+        elif instrument_name == 'timstof pro 2':
+            instrument_name = 'timsTOF Pro 2'
+        elif instrument_name == 'timstof flex':
+            instrument_name = 'timsTOF fleX'
+        elif instrument_name == 'timstof scp':
+            instrument_name = 'timsTOF SCP'
+        elif instrument_name == 'timstof ht':
+            instrument_name = 'Bruker Daltonics timsTOF series'  # placeholder since HT doesn't have CV param
+        elif instrument_name == 'timstof ultra':
+            instrument_name = 'timsTOF Ultra'
+        params = [instrument_name, {'instrument serial number': serial_number}]
+    else:
+        params = [{'instrument serial number': serial_number}]
+    # Data processing element.
+    if not barebones_metadata:
+        proc_methods = [writer.ProcessingMethod(order=1,
+                                                software_reference='psims-writer',
+                                                params=['Conversion to mzML']),
+                        writer.ProcessingMethod(order=2,
+                                                software_reference='TDF-SDK',
+                                                params=['Conversion to mzML']),
+                        writer.ProcessingMethod(order=3,
+                                                software_reference='flex_maldi_dda_automation',
+                                                params=['Conversion to mzML'])]
+        processing = writer.DataProcessing(proc_methods, id='exportation')
+        writer.data_processing_list([processing])
+    inst_config = writer.InstrumentConfiguration(id='instrument',
+                                                 component_list=[source, analyzer, detector],
+                                                 params=params)
+    writer.instrument_configuration_list([inst_config])
+
+
+def write_stitched_mzml(dlist, sorted_dlist_lower, outfile, mode, profile_bins, compression, encoding):
+    # initialize mzml writer
+    writer = MzMLWriter(outfile, close=True)
+    with writer:
+        # Write spectra list.
+        with writer.run(id='run',
+                        instrument_configuration='instrument',
+                        start_time=dlist[0].analysis['GlobalMetadata']['AcquisitionDateTime']):
+            scan_count = 0
+            with writer.spectrum_list(count=dlist[0].analysis['Frames'].shape[0]):
+                # code to parse datasets into TsfSpectrum and append numpy arrays
+                for frame in range(1, dlist[0].analysis['Frames'].shape[0] + 1):
+                    # get a maldiframeinfo dict
+                    maldiframeinfo_dict = dlist[0].analysis['MaldiFrameInfo'][dlist[0].analysis['MaldiFrameInfo']['Frame'] ==
+                                                                              frame].to_dict(orient='records')[0]
+                    # get, trim, and stitch spectra
+                    stitched_mz_array, stitched_intensity_array = i_activate_the_magic_card_polymerization(sorted_dlist_lower,
+                                                                                                           frame,
+                                                                                                           mode,
+                                                                                                           profile_bins,
+                                                                                                           encoding)
+                    # Build params list for spectrum.
+                    scan_count += 1
+                    base_peak_index = np.where(stitched_intensity_array == np.max(stitched_intensity_array))
+                    params = ['MS1 spectrum',
+                              {'ms level': 1},
+                              {'total ion current': sum(stitched_intensity_array)},
+                              {'base peak m/z': stitched_mz_array[base_peak_index][0].astype(float)},
+                              ({'name': 'base peak intensity',
+                                'unit_name': 'number of detector counts',
+                                'value': stitched_intensity_array[base_peak_index][0].astype(float)}),
+                              {'highest observed m/z': float(max(stitched_mz_array))},
+                              {'lowest observed m/z': float(max(stitched_mz_array))},
+                              {'maldi spot identifier': get_maldi_coords(dlist[0],
+                                                                         maldiframeinfo_dict)}]
+                    encoding_dict = {'m/z array': get_encoding_dtype(encoding),
+                                     'intensity array': get_encoding_dtype(encoding)}
+                    # Write MS1 spectrum
+                    writer.write_spectrum(stitched_mz_array,
+                                          stitched_intensity_array,
+                                          id='scan=' + str(scan_count),
+                                          polarity=list(set(dlist[0].analysis['Frames']['Polarity'].values.tolist()))[0],
+                                          centroided=get_centroid_status(mode)[0],
+                                          scan_start_time=0,
+                                          # other_arrays=None
+                                          params=params,
+                                          encoding=encoding_dict,
+                                          compression=compression)
+
+
+def main():
+    # tmp params
+    mode = 'profile'
+    profile_bins = 0
+    compression = 'zlib'
+    encoding = 64
+    barebones_metadata = False
+    filenames = ['C:\\Users\\bass\\data\\20240322_autox_windows\\strain1_liloandstitch_mr1.d',
+                 'C:\\Users\\bass\\data\\20240322_autox_windows\\strain1_liloandstitch_mr2.d',
+                 'C:\\Users\\bass\\data\\20240322_autox_windows\\strain1_liloandstitch_mr3.d']
+
+    # read in datasets
+    dll = init_tdf_sdk_api()
+    dlist = [PartialTsfData(dfile, dll) for dfile in filenames]
+    # group dataset into a list (random order)
+    masses = [x.MzAcqRangeLower for x in dlist] + [x.MzAcqRangeUpper for x in dlist]
+    sorted_dlist_lower = sorted(dlist, key=lambda x: x.MzAcqRangeLower)  # sort by low to high lower mass range
+    sorted_dlist_upper = sorted(dlist, key=lambda x: x.MzAcqRangeUpper)  # sort by low to high upper mass range
+
+    # logic check to make sure mass ranges don't overlap
+    # first check: make sure order of sorted lists are the same
+    if sorted_dlist_lower == sorted_dlist_upper:
+        # second check: make sure mass ranges don't overlap
+        for i in range(0, len(dlist) - 1):
+            if (not sorted_dlist_lower[i].MzAcqRangeUpper <= sorted_dlist_lower[i + 1].MzAcqRangeLower or
+                    not sorted_dlist_upper[i].MzAcqRangeUpper <= sorted_dlist_upper[i + 1].MzAcqRangeLower):
+                print('error')
+                sys.exit(1)
+            else:
+                # third check: make sure the first lower mass range is the lowest value of all and the last upper mass
+                # range is the largest
+                if (not sorted_dlist_lower[0].MzAcqRangeLower == min(masses) or
+                        not sorted_dlist_upper[0].MzAcqRangeLower == min(masses) or
+                        not sorted_dlist_lower[-1].MzAcqRangeUpper == max(masses) or
+                        not sorted_dlist_upper[-1].MzAcqRangeUpper == max(masses)):
+                    print('error')
+                    sys.exit(1)
+                else:
+                    # run the workflow
+                    write_stitched_mzml(dlist, 'C:\\Users\\bass\\data\\20240322_autox_windows\\test.mzML')
+
+    else:
+        print('error')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
